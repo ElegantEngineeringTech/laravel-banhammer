0423b7de0a2b268ee9a4ed8f16290c5cfa81410d
Quentin Gabriele <quentin.gabriele@gmail.com>
2025-05-16 18:24:12 +0200

    fix stan

--- src/Models/Contracts/BannableContract.php
+++ src/Models/Contracts/BannableContract.php
@@ -7,19 +7,20 @@ namespace Elegantly\Banhammer\Models\Contracts;
 use Carbon\Carbon;
 use Elegantly\Banhammer\Models\Ban;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User;
 
 /**
  * @template TBan of Ban
- *
+ * 
  * @property Collection<int, TBan> $bans
  * @property ?int $ban_level
  */
 interface BannableContract
 {
     /**
-     * @return MorphMany<TBan, $this>
+     * @return MorphMany<TBan, Model>
      */
     public function bans(): MorphMany;
 
